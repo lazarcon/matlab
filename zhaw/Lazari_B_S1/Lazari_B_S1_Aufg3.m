f69438691c38a94683c2855ef0fb0a5dcd23d63c
lazarcon <constantin.lazari@gmail.com>
2013-10-03 16:15:08 +0200

    Moved Series 1 Exercises to Lazari_B_S1

--- zhaw/Lazari_B_S1/Lazari_B_S1_Aufg3.m
+++ zhaw/Lazari_B_S1/Lazari_B_S1_Aufg3.m
@@ -17,13 +17,3 @@ if (t_rekursiv < t_for)
 else
     fprintf('Es gewinnt die for Variante. Sie ist %d Zeiteinheiten schneller.\n', t_for - t_rekursiv);
 end
-
-% Es gewinnt die Schleifen-Variante.
-% Dies wohl, weil sich der Computer bei der Rekursive Variante alle
-% Zwischenergebnisse bzw. unvollendete Instanzen quasi merken muss.
-% Ab 32 oder 64 Iterationen dürften dafür nicht mehr genügend
-% Register zur Verfügung stehen, und es muss in den Speicher ausgelagert
-% werden, was Zeit kostet.
-% Die Schleifen-Variante kommt mit 3 Variablen (Eingabe, Position, Wert) aus.
-% Es werden also auf Hardware-Ebene keine Speicherzugriffe benötigt,
-% Somit läuft der Prozess schneller.
